6eeaff1b6f4ea7b0e38270ca87842657db4952f1
Elena Justo <elenaj@protonmail.ch>
2023-09-13 21:36:57 +1000

    Skeleton header stuff

---
+++ mama.h
@@ -0,0 +1,24 @@
+// This is a library header file!
+
+/*
+This contains the definition of the class that
+implements the driver for our sensor :)
+*/
+
+#ifndef mama_h        // Checks to see if mama_h has been defined
+#define mama_h        // Defines mama_h if it has not been defined
+
+#include "Arduino.h"
+
+class mama {
+    public:
+        mama();        // Class constructor
+        ~mama();       // Class destructor
+        
+        // Do stuff
+
+    private:
+        // Do stuff
+};
+
+#endif
